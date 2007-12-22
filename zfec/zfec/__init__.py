8cda7eb4cd13edc0d542c2941b1d3e5d09eb5380
zooko <zooko@zooko.com>
2007-12-22 10:13:57 +0530

    zfec: setup: improve documentation in __init__.py
    darcs-hash:b686acf87ce884cd98c75119e5902352735d88f7

--- zfec/zfec/__init__.py
+++ zfec/zfec/__init__.py
@@ -10,8 +10,9 @@ __version__ = "unknown"
 try:
     from _version import __version__
 except ImportError:
-    # we're running in a tree that hasn't run darcsver.py, so we don't
-    # know what our version is. This should not happen very often.
+    # We're running in a tree that hasn't run darcsver from the pyutil library,
+    # and didn't come with a _version.py, so we don't know what our version
+    # is. This should not happen very often.
     pass
 
 from _fec import Encoder, Decoder, Error
