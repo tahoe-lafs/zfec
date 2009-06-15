0abdcd9d0a6a83993a4c586b3bc8c79296483b08
zooko <zooko@zooko.com>
2009-06-16 02:22:20 +0530

    setup: increase requirement on setuptools version to >= 0.6c10 (which means our own toothpick of setuptools a.k.a. zetuptoolz) because the setuptools-0.6c7 on our Windows buildslave has a problem with multiple overlapping dependencies on pyutil, but 0.6c10dev (which is already bundled with zfec) doesn't
    Ignore-this: e0aa17a5c769cdd427279ff60a4549e0
    
    darcs-hash:b2b4c229f9335617d1cbaba3364bb734c0e431f3

--- zfec/setup.py
+++ zfec/setup.py
@@ -19,7 +19,7 @@ except ImportError:
     pass
 else:
     # On cygwin there was a permissions error that was fixed in 0.6c6.
-    use_setuptools(min_version='0.6c6', download_delay=0, to_dir=miscdeps)
+    use_setuptools(min_version='0.6c10', download_delay=0, to_dir=miscdeps)
 
 from setuptools import Extension, find_packages, setup
 
