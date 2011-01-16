a891dd1d117a676d14fd5ca974644ea47fd10d4e
zooko <zooko@zooko.com>
2011-01-16 23:27:41 +0530

    setup: oh, and if there is a utf-8 "BOM" then decode the file contents as utf-8 before passing them to setup()
    Ignore-this: 1c9843b51010f397b451221e2774d26
    
    darcs-hash:eb197f870a35e036599d685de816d4df9af44522

--- zfec/setup.py
+++ zfec/setup.py
@@ -143,7 +143,7 @@ data_files = [(doc_loc, data_fnames)]
 readmetext = open('README.rst').read()
 if readmetext[:3] == '\xef\xbb\xbf':
     # utf-8 "BOM"
-    readmetext = readmetext[3:]
+    readmetext = readmetext[3:].decode('utf-8')
 
 setup(name=PKG,
       version=verstr,
