ea052a63bf05c6b572db3002097964f95dadd4f5
Vasudev Kamath <vasudev@copyninja.info>
2017-04-30 14:37:16 +0530

    Convert the package to use pbr

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+from setuptools.extension import Extension
+
+import sys
+
+DEBUGMODE = False
+
+if "--debug" in sys.argv:
+    DEBUGMODE = True
+    sys.argv.remove("--debug")
+
+extra_link_args = []
+extra_compile_args = []
+define_macros = []
+undef_macros = []
+
+for arg in sys.argv:
+    if arg.startswith("--strid="):
+        stride = int(arg[len("--stride="):])
+        define_macros.append(('STRIDE', stride))
+        sys.argv.remove(arg)
+        break
+
+extra_compile_args.append("-std=c99")
+if DEBUGMODE:
+    extra_compile_args.append("-O0")
+    extra_compile_args.append("-g")
+    extra_compile_args.append("-Wall")
+    extra_compile_args.append("-Wextra")
+    extra_link_args.append("-g")
+    undef_macros.append('NDEBUG')
+
+extensions = [
+    Extension(
+        "zfec._fec",
+        [
+            "zfec/fec.c",
+            "zfec/_fecmodule.c"
+        ],
+        include_dirs=["zfec/"],
+        extra_link_args=extra_link_args,
+        extra_compile_args=extra_compile_args,
+        define_macros=define_macros,
+        undef_macros=undef_macros
+    )
+]
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+    ext_modules=extensions
+)
