6063719bfb70552d6594300a8fb5ca2977c66524
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-12-23 15:58:04 -0500

    some fixes for python 3 compatibility
    However, this also depends on fixing some problems with pyutil.

--- bench/bench_zfec.py
+++ bench/bench_zfec.py
@@ -30,12 +30,12 @@ def _make_new_rand_data(size, k, m):
     blocksize = mathutil.div_ceil(size, k)
     for i in range(k):
         ds[i] = d[i*blocksize:(i+1)*blocksize]
-    ds[-1] = ds[-1] + "\x00" * (len(ds[-2]) - len(ds[-1]))
+    ds[-1] = ds[-1] + b"\x00" * (len(ds[-2]) - len(ds[-1]))
     easyfecenc = easyfec.Encoder(k, m)
     fecenc = Encoder(k, m)
 
-import sha
-hashers = [ sha.new() for i in range(M) ]
+from hashlib import sha1 as sha
+hashers = [ sha() for i in range(M) ]
 def hashem(results, reslenthing):
     for i, result in enumerate(results):
         hashers[i].update(result)
@@ -82,21 +82,21 @@ def bench(k, m):
     # for f in [_encode_file,]:
     # for f in [_encode_file_not_really, _encode_file_not_really_and_hash, _encode_file, _encode_file_and_hash,]:
     # for f in [_encode_data_not_really, _encode_data_easyfec, _encode_data_fec,]:
-    print "measuring encoding of data with K=%d, M=%d, reporting results in nanoseconds per byte after encoding %d bytes %d times in a row..." % (k, m, SIZE, MAXREPS)
+    print("measuring encoding of data with K=%d, M=%d, reporting results in nanoseconds per byte after encoding %d bytes %d times in a row..." % (k, m, SIZE, MAXREPS))
     # for f in [_encode_data_fec, _encode_data_not_really]:
     for f in [_encode_data_fec]:
         def _init_func(size):
             return _make_new_rand_data(size, k, m)
         for BSIZE in [SIZE]:
-            results = benchutil.rep_bench(f, n=BSIZE, initfunc=_init_func, MAXREPS=MAXREPS, MAXTIME=None, UNITS_PER_SECOND=1000000000)
-            print "and now represented in MB/s..."
-            print
+            results = benchutil.rep_bench(f, n=BSIZE, initfunc=_init_func, runreps=MAXREPS, UNITS_PER_SECOND=1000000000)
+            print("and now represented in MB/s...")
+            print()
             best = results['best']
             mean = results['mean']
             worst = results['worst']
-            print "best:  % 4.3f MB/sec" % (10**3 / best)
-            print "mean:  % 4.3f MB/sec" % (10**3 / mean)
-            print "worst: % 4.3f MB/sec" % (10**3 / worst)
+            print("best:  % 4.3f MB/sec" % (10**3 / best))
+            print("mean:  % 4.3f MB/sec" % (10**3 / mean))
+            print("worst: % 4.3f MB/sec" % (10**3 / worst))
 
 k = K
 m = M
