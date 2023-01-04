57341f79b51c4f47bbc30e0e5b03239c4b504cb1
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-01-04 15:10:30 -0500

    Add some more test cases for FEC.encode/decode
    Also add some comments to testFEC.

--- haskell/test/FECTest.hs
+++ haskell/test/FECTest.hs
@@ -5,6 +5,7 @@ module Main where
 
 import Test.Hspec
 
+import Data.Word
 import qualified Data.ByteString.Lazy as BL
 import qualified Codec.FEC as FEC
 import qualified Data.ByteString as B
@@ -13,8 +14,8 @@ import System.IO (IOMode (..), withFile)
 import System.Random
 import Data.Int
 import Data.Serializer
-
 import Test.QuickCheck
+
 newtype ArbByteString = ArbByteString BL.ByteString deriving newtype (Show, Ord, Eq)
 
 instance Arbitrary ArbByteString where
@@ -40,6 +41,11 @@ instance Arbitrary Params where
         total <- choose (min 255 (required + 1), 255)
         return $ Params required total
 
+instance Arbitrary FEC.FECParams where
+    arbitrary = do
+      (Params required total) <- arbitrary :: Gen Params
+      return $ FEC.fec required total
+
 randomTake :: Int -> Int -> [a] -> [a]
 randomTake seed n values = map snd $ take n sortedValues
   where
@@ -49,17 +55,38 @@ randomTake seed n values = map snd $ take n sortedValues
     rnds = randoms gen
     gen = mkStdGen seed
 
-testFEC :: Int -> Int -> Int -> Int -> Bool
-testFEC k n len seed = FEC.decode fec someTaggedBlocks == origBlocks
+-- | Any combination of the inputs blocks and the output blocks from
+-- @FEC.encode@, as long as there are at least @k@ of them, can be recombined
+-- using @FEC.decode@ to produce the original input blocks.
+--
+testFEC
+  :: FEC.FECParams
+  -- ^ The FEC parameters to exercise.
+  -> Word16
+  -- ^ The length of the blocks to exercise.
+  -> Int
+  -- ^ A random seed to use to be able to vary the choice of which blocks to
+  -- try to decode.
+  -> Bool
+  -- ^ True if the encoded input was reconstructed by decoding, False
+  -- otherwise.
+testFEC fec len seed = FEC.decode fec someTaggedBlocks == origBlocks
   where
-    origBlocks = map (\i -> B.replicate len $ fromIntegral i) [0 .. (k - 1)]
-    fec = FEC.fec k n
-    secondaryBlocks = FEC.encode fec origBlocks
-    taggedBlocks = zip [0 ..] (origBlocks ++ secondaryBlocks)
-    someTaggedBlocks = randomTake seed k taggedBlocks
+    -- Construct some blocks.  Each will just be the byte corresponding to the
+    -- block number repeated to satisfy the requested length.
+    origBlocks = B.replicate (fromIntegral len) . fromIntegral <$> [0 .. (FEC.paramK fec - 1)]
 
-prop_FEC :: Params -> Int -> Int -> Property
-prop_FEC (Params k n) len seed = len < 1024 ==> testFEC k n len seed
+    -- Encode the data to produce the "secondary" blocks which (might) add
+    -- redundancy to the original blocks.
+    secondaryBlocks = FEC.encode fec origBlocks
+
+    -- Tag each block with its block number because the decode API requires
+    -- this information.
+    taggedBlocks = zip [0 ..] (origBlocks ++ secondaryBlocks)
+
+    -- Choose enough of the tagged blocks (some combination of original and
+    -- secondary) to try to use for decoding.
+    someTaggedBlocks = randomTake seed (FEC.paramK fec) taggedBlocks
 
 checkDivide :: Int -> IO ()
 checkDivide n = do
@@ -69,6 +96,8 @@ checkDivide n = do
         then return ()
         else fail "checkDivide failed"
 
+prop_decode :: FEC.FECParams -> Word16 -> Int -> Property
+prop_decode fec len seed = len < 1024 ==> testFEC fec len seed
 
 prop_deFEC :: Params -> ArbByteString -> Property
 prop_deFEC (Params required total) (ArbByteString testdata) =
@@ -82,7 +111,10 @@ main :: IO ()
 main = hspec $ do
     describe "FEC" $ do
         it "secureCombine is the inverse of secureDivide n" $ mapM_ checkDivide [1, 2, 3, 4, 10]
-        it "decode is (nearly) the inverse of encode" $ (withMaxSuccess 5000 prop_FEC)
     describe "deFEC" $ do
         it "is the inverse of enFEC" $ (withMaxSuccess 2000 prop_deFEC)
 
+    describe "decode" $ do
+        it "is (nearly) the inverse of encode" $ (withMaxSuccess 2000 prop_decode)
+        it "works with total=256" $ property $ prop_decode (FEC.fec 1 256)
+        it "works with required=256" $ property $ prop_decode (FEC.fec 256 256)
