20afad550e0e928428c7e01de629d32803a771a2
Sajith Sasidharan <sajith@hcoop.net>
2026-03-24 10:35:27 -0500

    flake: use ghc9103 and bypass broken pre-commit devShell hook

--- flake.nix
+++ flake.nix
@@ -18,15 +18,28 @@
     ulib = flake-utils.lib;
   in
     ulib.eachSystem ["x86_64-linux"] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      hsPkgs = pkgs.haskell.packages.ghc9103;
       hslib = hs-flake-utils.lib {
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = pkgs;
         src = ./.;
         compilerVersion = "ghc9103";
         packageName = "fec";
       };
     in {
       checks = hslib.checks {};
-      devShells = hslib.devShells {};
+
+      devShells.default = hsPkgs.shellFor {
+        packages = p: [];
+        buildInputs = [
+          hsPkgs.cabal-install
+          hsPkgs.ghcid
+          hsPkgs.haskell-language-server
+          pkgs.gawk
+          pkgs.gnused
+        ];
+      };
+
       packages = hslib.packages {};
       apps = {
         hlint = hslib.apps.hlint {argv = ["haskell/"];};
