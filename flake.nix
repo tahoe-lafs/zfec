54286f29c562ed7eebb51bcf24a47f6a3c1db02e
Sajith Sasidharan <sajith@hcoop.net>
2026-06-01 16:27:20 -0500

    Re-enable packages

--- flake.nix
+++ flake.nix
@@ -79,8 +79,8 @@
               ];
             };
 
-          # # packages are auto-wired via haskell-flake
-          # packages = config.haskellProjects.default.outputs.packages;
+          # packages are auto-wired via haskell-flake
+          packages = config.haskellProjects.default.outputs.packages;
 
           checks = config.haskellProjects.default.outputs.checks;
           apps = (config.haskellProjects.default.outputs.apps or {}) // {
