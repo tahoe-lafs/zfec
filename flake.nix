d5835c77f4e0171a334c797ee029137164069bfe
Sajith Sasidharan <sajith@hcoop.net>
2026-06-01 14:35:37 -0500

    Replace hs-flake-utils with haskell-flake+flake-parts

--- flake.nix
+++ flake.nix
@@ -3,75 +3,75 @@
 
   inputs = {
     # Nix Inputs
-    nixpkgs.url = github:nixos/nixpkgs/?ref=nixos-26.05;
-    nixpkgs-old.url = github:nixos/nixpkgs/?ref=nixos-23.11;
-    flake-utils.url = github:numtide/flake-utils;
-    hs-flake-utils.url = "git+https://whetstone.private.storage/jcalderone/hs-flake-utils.git?ref=main";
-    hs-flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/?ref=nixos-26.05";
+    nixpkgs-old.url = "github:nixos/nixpkgs/?ref=nixos-23.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-old,
-    flake-utils,
-    hs-flake-utils,
-  }: let
-    ulib = flake-utils.lib;
-  in
-    ulib.eachSystem ["x86_64-linux"] (system: let
-      oldpkgs = nixpkgs-old.legacyPackages.${system};
-      pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
-        python39Packages = oldpkgs.python39Packages;
-      });
-      hsPkgs = pkgs.haskell.packages.ghc9103;
-      hslib = hs-flake-utils.lib {
-        pkgs = pkgs;
-        src = ./.;
-        compilerVersion = "ghc9103";
-        packageName = "fec";
-      };
-    in {
-      checks = hslib.checks {};
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixpkgs-old,
+      flake-parts,
+      haskell-flake,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # Systems we support
+      systems = [ "x86_64-linux" ];
 
-      devShells.default = hsPkgs.shellFor {
-        packages = p: [];
-        buildInputs = [
-          hsPkgs.cabal-install
-          hsPkgs.ghcid
-          hsPkgs.haskell-language-server
-          pkgs.gawk
-          pkgs.gnused
+      perSystem =
+        { system, pkgs, ... }:
+        let
+          # Bring in the older python packages we need
+          pkgsOld = nixpkgs-old.legacyPackages.${system};
+          pkgs' = pkgs.extend (
+            self: super: {
+              python39Packages = pkgsOld.python39Packages;
+              python39 = pkgsOld.python39;
+              python310 = pkgsOld.python310;
+              pypy39 = pkgsOld.pypy39;
+            }
+          );
+          # Haskell utilities from haskell-flake
+          hs = haskell-flake.lib { inherit pkgs'; };
+        in
+        {
+          # Development shell – automatically picks up the Haskell project in this repo
+          devShells.default =
+            let
+              hspkgs = pkgs'.haskell.packages.ghc9103;
+            in
+            pkgs'.mkShell {
+              buildInputs = [
+                hspkgs.cabal-install
+                hspkgs.ghcid
+                hspkgs.haskell-language-server
 
-          # Python/PyPy matrix (current channel)
-          pkgs.python311
-          pkgs.python312
-          pkgs.python313
-          pkgs.python314
-          pkgs.pypy310
+                pkgs'.gawk
+                pkgs'.gnused
 
-          # Python/PyPy matrix extras from older channel
-          oldpkgs.python39
-          oldpkgs.python310
-          oldpkgs.pypy39
+                # Python/PyPy matrix from the current channel.
+                pkgs'.python311
+                pkgs'.python312
+                pkgs'.python313
+                pkgs'.python314
+                pkgs'.pypy310
+                pkgs'.python3Packages.tox
 
-          # The tox test tool
-          pkgs.python3Packages.tox
-        ];
-      };
+                # Python/PyPy matrix extras from older channel.
+                pkgs'.python39
+                pkgs'.python310
+                pkgs'.pypy39
+              ];
+            };
 
-      packages = hslib.packages {};
-      apps = {
-        hlint = hslib.apps.hlint {argv = ["haskell/"];};
-        cabal-test = hslib.apps.cabal-test {
-          extraRuntimeInputs = pkgs: [
-            # Some build-time dependencies of old-time, a transitive
-            # dependency of ours...
-            pkgs.gnused
-            pkgs.gawk
-          ];
-          testTargetName = "test:tests";
+          # Expose the Haskell packages (empty placeholder for now)
+          packages = { };
+          checks = { };
+          apps = { };
         };
-      };
-    });
+    };
 }
